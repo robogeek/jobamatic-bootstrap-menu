eb50fcfd2a990d2f7b426058f7cc339e6309a101
David Herron <david@davidherron.com>
2013-02-24 16:47:50 -0800

    Update

---
+++ config-sample.js
@@ -0,0 +1,127 @@
+module.exports = {
+    analytics: {
+        doit: true,
+        account: "UA-NNNNNNN",
+        domain: "example.com"
+    },
+    sharethis: {
+        doit: true,
+        publisher: "hex-code-from-sharethis",
+        position: "left|right .. etc.."
+    },
+    tabs: [
+        {
+            active: true,
+            title: "Category 1",
+            machineName: "categ1",
+            tabs: [
+                {
+                    active: true,
+                    title: "Group Name 1",
+                    machineName: "name1",
+                    links: [
+                        { query: "job-query-1", title: "Job Title 1"},
+                        { query: "job-query-2", title: "Job Title 2"},
+                        { query: "job-query-3", title: "Job Title 3"},
+                    ]
+                },
+                {
+                    active: false,
+                    title: "Group Name 2",
+                    machineName: "name2",
+                    links: [
+                        { query: "job-query-1", title: "Job Title 1"},
+                        { query: "job-query-2", title: "Job Title 2"},
+                        { query: "job-query-3", title: "Job Title 3"},
+                    ]
+                },
+                {
+                    active: false,
+                    title: "Group Name 3",
+                    machineName: "name3",
+                    links: [
+                        { query: "job-query-1", title: "Job Title 1"},
+                        { query: "job-query-2", title: "Job Title 2"},
+                        { query: "job-query-3", title: "Job Title 3"},
+                    ]
+                }
+            ]
+        },
+        {
+            active: false,
+            title: "Category 2",
+            machineName: "categ2",
+            tabs: [
+                {
+                    active: true,
+                    title: "Group Name 1",
+                    machineName: "name1",
+                    links: [
+                        { query: "job-query-1", title: "Job Title 1"},
+                        { query: "job-query-2", title: "Job Title 2"},
+                        { query: "job-query-3", title: "Job Title 3"},
+                    ]
+                },
+                {
+                    active: false,
+                    title: "Group Name 2",
+                    machineName: "name2",
+                    links: [
+                        { query: "job-query-1", title: "Job Title 1"},
+                        { query: "job-query-2", title: "Job Title 2"},
+                        { query: "job-query-3", title: "Job Title 3"},
+                    ]
+                },
+                {
+                    active: false,
+                    title: "Group Name 3",
+                    machineName: "name3",
+                    links: [
+                        { query: "job-query-1", title: "Job Title 1"},
+                        { query: "job-query-2", title: "Job Title 2"},
+                        { query: "job-query-3", title: "Job Title 3"},
+                    ]
+                }
+                
+            ]
+        },
+        {
+            active: false,
+            title: "Category 3",
+            machineName: "categ3",
+            tabs: [
+                {
+                    active: true,
+                    title: "Group Name 1",
+                    machineName: "name1",
+                    links: [
+                        { query: "job-query-1", title: "Job Title 1"},
+                        { query: "job-query-2", title: "Job Title 2"},
+                        { query: "job-query-3", title: "Job Title 3"},
+                    ]
+                },
+                {
+                    active: false,
+                    title: "Group Name 2",
+                    machineName: "name2",
+                    links: [
+                        { query: "job-query-1", title: "Job Title 1"},
+                        { query: "job-query-2", title: "Job Title 2"},
+                        { query: "job-query-3", title: "Job Title 3"},
+                    ]
+                },
+                {
+                    active: false,
+                    title: "Group Name 3",
+                    machineName: "name3",
+                    links: [
+                        { query: "job-query-1", title: "Job Title 1"},
+                        { query: "job-query-2", title: "Job Title 2"},
+                        { query: "job-query-3", title: "Job Title 3"},
+                    ]
+                }
+                
+            ]
+        },
+    ]
+};
